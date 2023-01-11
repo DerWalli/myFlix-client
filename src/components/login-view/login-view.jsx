2cadd4d3292296ced0e80a44ce831110a2b9a135
DerWalli <33699620+DerWalli@users.noreply.github.com>
2023-01-11 20:53:56 +0100

    login-view, signup-view, reenabled authentification

---
+++ src/components/login-view/login-view.jsx
@@ -0,0 +1,53 @@
+import React from "react";
+import { useState } from "react";
+
+export const LoginView = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const handleSubmit = (event) => {
+    // this prevents the default behavior of the form which is to reload the entire page
+    event.preventDefault(); 
+  
+  
+  const data = {
+    Username: username,
+    Password: password
+  };
+
+  fetch("https://myflix-api-3dxz.onrender.com/login", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(data)
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      console.log("Login response: ", data);
+      if (data.user) {
+        localStorage.setItem("user", JSON.stringify(data.user));
+        localStorage.setItem("token", data.token);
+        onLoggedIn(data.user, data.token);
+      } else {
+        alert("No such user");
+      }
+    })
+    .catch((e) => {
+      alert("Something went wrong");
+    });
+  }
+  
+  return (
+      <form>
+        <label>
+          Username:
+          <input type="text" />
+        </label>
+        <label>
+          Password:
+          <input type="password" />
+        </label>
+        <button type="submit">Submit</button>
+      </form>
+    );
+  };
