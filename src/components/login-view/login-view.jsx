70acae42873e59857d130ac0bbd658a78f76a041
DerWalli <33699620+DerWalli@users.noreply.github.com>
2023-01-12 11:34:51 +0100

    update

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -4,48 +4,56 @@ import { useState } from "react";
 export const LoginView = () => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+ 
   const handleSubmit = (event) => {
-    // this prevents the default behavior of the form which is to reload the entire page
+    
     event.preventDefault(); 
   
-  
-  const data = {
-    Username: username,
-    Password: password
-  };
+    const data = {
+      Username: username,
+      Password: password
+    };
 
-  fetch("https://myflix-api-3dxz.onrender.com/login", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json"
-    },
-    body: JSON.stringify(data)
-  })
-    .then((response) => response.json())
-    .then((data) => {
-      console.log("Login response: ", data);
-      if (data.user) {
-        localStorage.setItem("user", JSON.stringify(data.user));
-        localStorage.setItem("token", data.token);
-        onLoggedIn(data.user, data.token);
-      } else {
-        alert("No such user");
-      }
+    fetch("https://myflix-api-3dxz.onrender.com/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(data)
     })
-    .catch((e) => {
-      alert("Something went wrong");
-    });
-  }
+      .then((response) => response.json())
+      .then((data) => {
+        console.log("Login response: ", data);
+        if (data.user) {
+          localStorage.setItem("user", JSON.stringify(data.user));
+          localStorage.setItem("token", data.token);
+          onLoggedIn(data.user, data.token);
+        } else {
+          alert("No such user");
+        }
+      })
+      .catch((e) => {
+        alert("Something went wrong");
+      });
+    }
   
   return (
-      <form>
+      <form onSubmit={handleSubmit}>
         <label>
           Username:
-          <input type="text" />
+          <input 
+          type="text"
+          value= {username}
+          onChange={(e) => setUsername(e.target.value)}
+           />
         </label>
         <label>
           Password:
-          <input type="password" />
+          <input
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          />
         </label>
         <button type="submit">Submit</button>
       </form>
