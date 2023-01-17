fe26946dadb8515b2dd2a53139de2cbde0cda658
DerWalli <33699620+DerWalli@users.noreply.github.com>
2023-01-17 19:27:04 +0100

    React-Bootstrap

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,27 +1,33 @@
-//
+import React from "react";
 import PropTypes from "prop-types";
+import { Button, Card } from "react-bootstrap";
 
-//
+
 export const MovieCard = ({ movie, onMovieClick }) => {
-  console.log(1)
-    return (
-      <div
-        onClick={() => {
-          onMovieClick(movie);
-        }}
-      >
-        {movie.Title}
-        
-      </div>
-    );
-  };
+  console.log("MovieCard", movie.title)
+    return ( 
+      <Card onClick={() => onMovieClick(movie)}>
+      <Card.Img variant="top" src={movie.image} />
+      <Card.Body>
+        <Card.Title>{movie.title}</Card.Title>
+        <Card.Text>{movie.Director}</Card.Text>
+        <Button 
+          onClick={() => {
+            onMovieClick(movie);
+          }}
+        >Open
+        </Button>
+      </Card.Body>
+    </Card>
+  );
+};
    
 //here is where we define all the props constraints for the MovieCard
 MovieCard.propTypes = {
   movie: PropTypes.shape({
-    Title: PropTypes.string.isRequired
-//    image: PropTypes.string.isRequired,
-//    director: PropTypes.string
+    Title: PropTypes.string,
+    image: PropTypes.string,
+  //  director: PropTypes.string
   }).isRequired,
   onMovieClick: PropTypes.func.isRequired
 }; 
--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -1,20 +1,36 @@
+import "./movie-view.scss";
+
 export const MovieView = ({ movie, onBackClick }) => {
-    console.log(movie)
+  console.log(movie)
     return (
+      
       <div>
         <div>
-          <img src={movie.image} alt="the Movieposter" />
+          <img ClassName="w-100" src={movie.image} />
         </div>
         <div>
-          <span> Title: </span>
-          <span>{movie.Title}</span>
+          <span>Title: </span>
+          <span>{movie.title}</span>
         </div>
         <div>
-          <span> Author: </span>
-          <span>{movie.director}</span>
+          <span>Director: </span>
+          <span>{movie.director.Name}</span>
         </div>
-        <button onClick={onBackClick}>Back</button>
+        <div>
+          <span>Genre: </span>
+          <span>{movie.genre.Name}</span>
+        </div>
+        <div>
+          <span>Description: </span>
+          <span>{movie.description}</span>
+        </div>
+        <button 
+        onClick={onBackClick}
+        className="back-button"
+        style={{ cursor: "pointer" }}
+        >Back</button>
       </div>
     );
-  };  
+  };
+  
   
